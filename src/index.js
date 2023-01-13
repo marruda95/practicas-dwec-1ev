4d934506251c8cbc6886e84b06c24b2654df3996
Maria Arruda <Maria.Arruda@adidas.com>
2023-01-13 19:30:42 +0100

    Add table + Reset function

--- src/index.js
+++ src/index.js
@@ -8,37 +8,41 @@ window.onload = () => {
     let positionRow 
     let playerRow 
     let count = 0
+    let currentPlayer = true
     const wins = [
         // ROWS
-        ["cell-1-1", "cell-1-2", "cell-1-3"],
-        ["cell-2-1", "cell-2-2", "cell-2-3"],
-        ["cell-3-1", "cell-3-2", "cell-3-3"],
-        // COLUMNS 
-        ["cell-1-1", "cell-2-1", "cell-3-1"],
-        ["cell-1-2", "cell-2-2", "cell-3-2"],
-        ["cell-1-3", "cell-2-3", "cell-3-3"],
-        // DIAGONAL
-        ["cell-1-1", "cell-2-2", "cell-3-3"],
-        ["cell-1-3", "cell-2-2", "cell-3-1"]
+        ['1-1', '1-2', '1-3']
     ]
     let player_o = []
     let player_x = []
-    let selectedCell = []
-   
+    let selectedCell
+    let table = document.getElementById('movements')
+    console.log(currentPlayer)
     for (const cell of cells) {
-        let table = document.getElementById('movements')
-        
-        
         cell.onclick = (event) => {
             const [, x, y] = event.target.id.split('-')
+
+            // COUNT TURNS
             count++
+
+            // SAVE SELECTED CELL 
             selectedCell = `${x}-${y}`
 
-            // When the cell is already being used
-            if (cell.className === "cell-x" || cell.className === "cell-o") 
+            // INSERT MOVEMENT ROW
+            newRow = table.insertRow(0)
+            playerRow = newRow.insertCell(-1) 
+            turnRow = newRow.insertCell(0)
+            positionRow = newRow.insertCell(1)
+            turnRow.innerHTML = count
+            positionRow.innerHTML = selectedCell
+            
+            // CELL IS ALREADY MARKED
+            if (cell.className === "cell-x" || cell.className === "cell-o") {
                 window.alert("Oops, you can't place it here. You lose a turn!")
+                positionRow.innerHTML = "Skipped"
+            }
 
-            // O
+            // PLAYRER O
             if (players.className === "turn-cell-o"){
                 // PLACE MARKER
                 if (cell.className === 'cell')
@@ -49,57 +53,58 @@ window.onload = () => {
                 players.classList.add("turn-cell-x")
 
                 // SAVE MOVEMENT
-                player_o = [selectedCell]
+                player_o.push(selectedCell)
                 console.log(player_o)
-
-                // INSERT MOVEMENT
-                newRow = table.insertRow(0)
-                playerRow = newRow.insertCell(-1) 
-                turnRow = newRow.insertCell(0)
-                positionRow = newRow.insertCell(1)
-
-                // FILL INFORMATION
-                turnRow.innerHTML = count
-                positionRow.innerHTML = selectedCell
                 playerRow.innerHTML = "O"
-                    
-                    
+
+                currentPlayer = !currentPlayer
+                console.log(currentPlayer)
             }
-            // X
+
+            // PLAYER X
             else if (players.className === "turn-cell-x"){
                 
+                // PLACE MARKER
                 if (cell.className === 'cell')
                     cell.className = 'cell-x'
 
                 // CHANGE PLAYER 
                 players.classList.remove("turn-cell-x")
                 players.classList.add("turn-cell-o")
-                player_x = [selectedCell]
-                console.log(player_x)
-
-                // INSERT MOVEMENT
-                newRow = table.insertRow(0)
-                playerRow = newRow.insertCell(-1) 
-                turnRow = newRow.insertCell(0)
-                positionRow = newRow.insertCell(1)
                 
-                // FILL INFORMATION
-                turnRow.innerHTML = count
-                positionRow.innerHTML = selectedCell
-                playerRow.innerHTML = "X"
+                
+                playerRow.innerHTML = "X"    
+                currentPlayer = !currentPlayer // true
+                
+                player_x.push(selectedCell)
+                console.log(player_x)
+                console.log(currentPlayer)
             } 
-            // WIN
-            // for each win in wins
-
-
-            // RESTART GAME: all elements purple
-
-            // ADD MOVEMENTS TO LOG:
-            // - add placement
-            // - add player
-            // - add movement number
-              
-        }        
+            
+        }  
+        
+        
+        
+    }
+    // WIN 
+    
+ 
+    // RESTART ENTIRE GAME
+    restart.onclick = function() {restartGame()} 
+    function restartGame() { 
+        // REMOVE MOVEMENTS REGISTERS
+        table.innerHTML = ""
+        // UNMARK CELLS 
+        let unmark = document.getElementsByName('unmark')
+        unmark.forEach(cell => {
+            cell.classList.remove("cell-o")
+            cell.classList.remove("cell-x")
+            cell.classList.add("cell")
+        })
+        // RESTART COUNT
+        count = 0
+        // CHANGE PLAYER TO X
+        players.className = "turn-cell-x"
     }
 
 }
