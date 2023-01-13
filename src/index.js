ebd4f31f0e685922b4956fc5fc35c304b5f566c9
Maria Arruda <Maria.Arruda@adidas.com>
2023-01-13 13:33:49 +0100

    Add turn function + no repeating cells

--- src/index.js
+++ src/index.js
@@ -3,39 +3,36 @@ window.onload = () => {
 
     let cells = document.getElementsByClassName('cell')
     let players = document.getElementById('player')
+   
     for (const cell of cells) {
         cell.onclick = (event) => {
             const [, x, y] = event.target.id.split('-')
             console.log(`click on ${x}:${y}`)
 
-            // PLAYER O X
+            // When the cell is already being used
+            if (cell.className === "cell-x" || cell.className === "cell-o") 
+                window.alert("Oops, you can't place it here. You lose a turn!")
+
+            // O
+            if (players.className === "turn-cell-o"){
+                if (cell.className === 'cell')
+                    cell.className = 'cell-o'
+                    players.classList.remove("turn-cell-o")
+                    players.classList.add("turn-cell-x")
+            }
+            // X
+            else if (players.className === "turn-cell-x"){
+                if (cell.className === 'cell')
+                    cell.className = 'cell-x'
+                    players.classList.remove("turn-cell-x")
+                    players.classList.add("turn-cell-o")
+            } 
             
-            // if (player.className === 'turn-cell-o')
-                
+            // RESTART GAME: all elements purple
             
-            if (cell.className === 'cell')
-                cell.className = 'cell-x'
-
-            if (cell.className === 'cell-x')
-                cell.className = 'cell-x'
+           
             
-            if (cell.className === 'cell-o')
-                cell.className = 'cell-o'
-
-
-            // else {
-            //     if (cell.className === 'cell-o')
-            //         cell.className = 'cell-x'
-            //     else
-            //         cell.className = 'cell-o'
-            // }
-        }
-
-        // RESTART GAME: all elements purple
-        restart.onclick = (event) => {
-            cell.className = 'cell'
-        }
- }
-
+        }        
+    }
 
 }
