0474d7f29855eb67db96207500646d24e3be42ca
Maria Arruda <Maria.Arruda@adidas.com>
2023-01-13 12:31:36 +0100

    Map cells and players

--- src/index.js
+++ src/index.js
@@ -1,20 +1,41 @@
 window.onload = () => {
     console.log('loaded')
 
-    // let cells = <GET ALL cell ELEMENTS>
-    let cell = document.getElementsByClassName('cell-x')[0]
-    // for (const cell of cells) {
+    let cells = document.getElementsByClassName('cell')
+    let players = document.getElementById('player')
+    for (const cell of cells) {
         cell.onclick = (event) => {
             const [, x, y] = event.target.id.split('-')
             console.log(`click on ${x}:${y}`)
+
+            // PLAYER O X
+            
+            // if (player.className === 'turn-cell-o')
+                
+            
             if (cell.className === 'cell')
+                cell.className = 'cell-x'
+
+            if (cell.className === 'cell-x')
+                cell.className = 'cell-x'
+            
+            if (cell.className === 'cell-o')
                 cell.className = 'cell-o'
-            else {
-                if (cell.className === 'cell-o')
-                    cell.className = 'cell-x'
-                else
-                    cell.className = 'cell-o'
-            }
+
+
+            // else {
+            //     if (cell.className === 'cell-o')
+            //         cell.className = 'cell-x'
+            //     else
+            //         cell.className = 'cell-o'
+            // }
         }
-    // }
+
+        // RESTART GAME: all elements purple
+        restart.onclick = (event) => {
+            cell.className = 'cell'
+        }
+ }
+
+
 }
